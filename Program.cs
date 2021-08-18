7dbc5fc41554308bdaacad10920fef1b94568231
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-18 13:19:13 -0500

    First Commit for Intro to C# with Command Line

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace dotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine( "Intro to C# with Command Line");
+        }
+
+    }
+
+
+
+}
