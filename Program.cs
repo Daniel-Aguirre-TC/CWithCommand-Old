0e13ef9bba297a9aafd6d8c2a3a75b094f99e374
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-18 13:56:15 -0500

    GetInput() Method Created

--- Program.cs
+++ Program.cs
@@ -7,11 +7,37 @@ namespace dotNetConsole
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
             System.Console.WriteLine( "Intro to C# with Command Line");
+
+            System.Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            program.GetInput();
+            Console.ReadLine();
         }
 
+        public void GetInput()
+        {
+            System.Console.WriteLine("Please select one of the following:\n1) Command\n2) Github\n3) DotNot\n");
+            string input = Console.ReadLine();
+            switch(input){
+                case "1":
+                System.Console.WriteLine("You have selected Command.");
+                break;
+                case "2":
+                System.Console.WriteLine("You have selected Github.");
+                break;
+                case "3":
+                System.Console.WriteLine("You have selected DotNot.");
+                break;
+                default:
+                System.Console.WriteLine("Invalid Selection.\nPlease Select a valid option.");
+                System.Console.ReadLine();
+                System.Console.Clear();
+                GetInput();
+                break;
+            }
+        }
     }
-
-
-
 }
