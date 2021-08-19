7d5934fbc8e9101e2fb42f7274a43b25f1f1df13
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-19 11:56:50 -0500

    Added feature to close application with E during any step.

--- Program.cs
+++ Program.cs
@@ -6,26 +6,28 @@ namespace dotNetConsole
 {
     class Program
     {
-                    bool programActive = true;
+        bool programActive = true;
         static void Main(string[] args)
         {
             Program program = new Program();
             Console.Clear();
-            System.Console.WriteLine( "Intro to C# with Command Line\n\nPlease Press any key to continue.\n\nCreated by: Daniel Aguirre");
-            Console.ReadKey();
-            do{
-            Console.Clear();
-            // Ask user which commands to display, then show commands upon completion.
+            System.Console.WriteLine( "Intro to C# with Command Line\n\nIf you wish to end the application the enter E at any time.\n\nPlease Press any key to continue.\n\nCreated by: Daniel Aguirre");
+            program.ReadAndClear();          
+            while (program.programActive)
+            {
+                Console.Clear();
+                // Ask user which commands to display, then show commands upon completion.
                 program.GetInput();
-            // wait for Console.ReadKey() then will Console.Clear()
+                // wait for Console.ReadKey() then will Console.Clear()
                 program.ReadAndClear();
-            // Offer new command list, if user inputs no then will end program.
+                // Offer new command list, if user inputs no then will end program.
                 program.OfferNewCommandList();
-            }            
-            while (program.programActive);           
+            };           
         }
         public void OfferNewCommandList()
         {
+            if (programActive)
+            {
             System.Console.WriteLine("Would you like to view a new list of commands?\n\nPlease enter y/n");
             string input = Console.ReadLine();           
             Console.Clear();
@@ -37,10 +39,10 @@ namespace dotNetConsole
                 break;
                 // if no thank for using and break without calling new method. This should be the end of the program.
                 case "n":
-                System.Console.WriteLine("Thank you for using this Console Application!\n\nAny/All Feedback Welcome!\n\nCreated by: Daniel Aguirre\n\nPress any key to end.");
-                System.Console.ReadKey();
-                programActive = false;
-                Console.Clear();
+                EndApplication();
+                break;
+                case "e":
+                EndApplication();
                 break;
                 // if answer does not match, try again.
                 default:
@@ -49,34 +51,65 @@ namespace dotNetConsole
                 OfferNewCommandList();
                 break;
             }
+
+        }   
+    }
+        public void EndApplication()
+        {
+            Console.Clear();
+            System.Console.WriteLine("Thank you for using this Console Application!\n\nAny/All Feedback Welcome!\n\nCreated by: Daniel Aguirre\n\nPress any key to end.");
+            System.Console.ReadKey();
+            // setting programActive to false so that while loop will cancel.
+            programActive = false;
+            Console.Clear();
         }
+
         public void ReadAndClear()
         {
-            Console.ReadKey();
-            Console.Clear();
+            if (programActive)
+            {
+            ConsoleKeyInfo input = Console.ReadKey();
+            if(input.KeyChar == 'e')
+            {
+                EndApplication();
+            }
+            else Console.Clear();
+            }
         }
         public void GetInput()
         {
             System.Console.WriteLine("Please select one of the following:\n\n1) Command\n2) Github\n3) DotNet\n");
-            string input = Console.ReadLine();
-            switch(input){
-                case "1":
+            ConsoleKeyInfo input = Console.ReadKey();
+            switch(input.KeyChar){
+                case 'e':
+                EndApplication();
+                break;
+                case '1':
                 System.Console.WriteLine("\nYou have selected Command.");
                 ReadAndClear();
+                if (programActive)
+                {
                 System.Console.WriteLine("Console Commands:\n----------------------\n");
                 CommandLists.DisplayCommands(CommandLists.consoleList);
+                }
                 break;
-                case "2":
+                case '2':
                 System.Console.WriteLine("\nYou have selected Github.");
                 ReadAndClear();
+                if (programActive)
+                {
                 System.Console.WriteLine("Github Commands:\n----------------------\n");
                 CommandLists.DisplayCommands(CommandLists.githubList);
+                }
                 break;
-                case "3":
+                case '3':
                 System.Console.WriteLine("\nYou have selected DotNet.");                
                 ReadAndClear();
+                if (programActive)
+                {
                 System.Console.WriteLine("DotNet Commands:\n----------------------\n");
                 CommandLists.DisplayCommands(CommandLists.dotnetList);
+                }
                 break;
                 default:
                 System.Console.WriteLine("\nInvalid Selection.\nPlease Select a valid option.");
@@ -134,7 +167,7 @@ namespace dotNetConsole
         internal static void DisplayCommands(List<Command> commandsList)
         {
             foreach(Command cmd in commandsList)
-            {                
+            {                                
             System.Console.WriteLine(cmd.name +"\n- " + cmd.desc + "\n");
             }
         }
