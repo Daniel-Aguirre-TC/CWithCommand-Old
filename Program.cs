8d59623c57b7a9f610d9639f46c7b3875bce79bc
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-18 22:42:26 -0500

    Created Command and CommandLists classes to reduce repetitive code and allow new commands to be created more easily.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace dotNetConsole
 {
     class Program
     {
-            bool programActive = true;
+                    bool programActive = true;
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -21,11 +22,8 @@ namespace dotNetConsole
             // Offer new command list, if user inputs no then will end program.
                 program.OfferNewCommandList();
             }            
-            while (program.programActive);
-            
-
+            while (program.programActive);           
         }
-
         public void OfferNewCommandList()
         {
             System.Console.WriteLine("Would you like to view a new list of commands?\n\nPlease enter y/n");
@@ -42,6 +40,7 @@ namespace dotNetConsole
                 System.Console.WriteLine("Thank you for using this Console Application!\n\nAny/All Feedback Welcome!\n\nCreated by: Daniel Aguirre\n\nPress any key to end.");
                 System.Console.ReadKey();
                 programActive = false;
+                Console.Clear();
                 break;
                 // if answer does not match, try again.
                 default:
@@ -51,13 +50,11 @@ namespace dotNetConsole
                 break;
             }
         }
-
         public void ReadAndClear()
         {
             Console.ReadKey();
             Console.Clear();
         }
-
         public void GetInput()
         {
             System.Console.WriteLine("Please select one of the following:\n\n1) Command\n2) Github\n3) DotNet\n");
@@ -66,17 +63,20 @@ namespace dotNetConsole
                 case "1":
                 System.Console.WriteLine("\nYou have selected Command.");
                 ReadAndClear();
-                ShowCommands();
+                System.Console.WriteLine("Console Commands:\n----------------------\n");
+                CommandLists.DisplayCommands(CommandLists.consoleList);
                 break;
                 case "2":
                 System.Console.WriteLine("\nYou have selected Github.");
                 ReadAndClear();
-                ShowGit();
+                System.Console.WriteLine("Github Commands:\n----------------------\n");
+                CommandLists.DisplayCommands(CommandLists.githubList);
                 break;
                 case "3":
                 System.Console.WriteLine("\nYou have selected DotNet.");                
                 ReadAndClear();
-                ShowDotNet();
+                System.Console.WriteLine("DotNet Commands:\n----------------------\n");
+                CommandLists.DisplayCommands(CommandLists.dotnetList);
                 break;
                 default:
                 System.Console.WriteLine("\nInvalid Selection.\nPlease Select a valid option.");
@@ -84,45 +84,59 @@ namespace dotNetConsole
                 GetInput();
                 break;
             }
-        }
-        public void ShowCommands()
+        }       
+    }
+    class Command
+    {
+        public string name;
+        public string desc;
+        public Command(string Name, string Desc)
         {
-            System.Console.WriteLine("Console Commands:\n----------------------\n");
-            System.Console.WriteLine("code . \n- Open the current directory in Visual Code\n");
-            System.Console.WriteLine("rename <fileName> <newFileName>\n- Rename a file.\n");
-            System.Console.WriteLine("del <fileName> \n- Delete the file provided.\n");
-            System.Console.WriteLine("cls \n- Clear the screen.\n");
-            System.Console.WriteLine("mkdir <newDirectoryName> \n- Make a new directory with the provided name.\n");
-            System.Console.WriteLine("cd <directoryNameMovingTo> \n- Move into the directory provided\n");
-            System.Console.WriteLine("cd .. \n- Move into the parent directory of the one you're currently in.\n");
-            System.Console.WriteLine("rmdir <directoryNameDeleting> \n- Delete the directory provided. Must be in the parent directory.\n");
+            name = Name;
+            desc = Desc;
         }
-        public void ShowGit()
-        {        
-            Console.Clear();
-            System.Console.WriteLine("Github Commands:\n----------------------\n");
-            System.Console.WriteLine("git clone <CloneUrl> \n- Clone a local repo of the provided URL to your machine.\n");
-            System.Console.WriteLine("git checkout <BranchToSwitchTo> \n- Switch to the branch provided. If you do not stage any changes made before switching branches, then your changes will switch with you. To prevent this you should stage changes before checkout to a new branch.\n");
-            System.Console.WriteLine("git add . \n- Stage all files in the current directory.\n");
-            System.Console.WriteLine("git add <DirName> \n- Stage all files in the specified directory.\n");
-            System.Console.WriteLine("git commit -m 'message' \n- Commit staged files so that they can be pushed.\n");
-            System.Console.WriteLine("git push \n- Push all commited changes to the remote repo\n");
-            System.Console.WriteLine("git branch \n- List all branches in your local repo\n");
-            System.Console.WriteLine("git branch -a \n- Lists all branches even in the remote repo");
-            System.Console.WriteLine("git merge --abort \n- Cancel a pending merge\n");
-            System.Console.WriteLine("git remote set-url origin <url> \n- Reset the origin or remote repo\n");
-        }
-        public void ShowDotNet()
+    }
+    public static class CommandLists
+    {
+        internal static List<Command> consoleList = new List<Command>(){
+            new Command("code .", "Open the current directory in Visual Code."),
+            new Command("cd <directoryNameMovingTo>", "Move into the directory provided"),
+            new Command("cd ..", "Move into the parent directory of the one you're currently in."),
+            new Command("rename <fileName> <newFileName>", "Rename a specified file."),
+            new Command("del <fileName>", "Delete the file provided."),
+            new Command("cls", "Clear the screen."),
+            new Command("mkdir <newDirectoryName>", "Make a new directory with the provided name."),
+            new Command("rmdir <directoryNameDeleting>", "Delete the directory provided. Must be in the parent directory.")
+        };
+        internal static List<Command> githubList = new List<Command>()
         {
-            System.Console.WriteLine("DotNet Commands:\n----------------------\n");
-            System.Console.WriteLine("dotnet new \n- add a new solution file to the current directory.\n");
-            System.Console.WriteLine("dotnet new console \n- Create a new console project inside the current directory.\n");
-            System.Console.WriteLine("dotnet build \n- Builds the project and dependencies to ensure you are up to date.\n");
-            System.Console.WriteLine("dotnet run \n- Run your source code directly from the command prompt.\n");
-            System.Console.WriteLine("dotnet sln \n- Sln allows you to add, remove, and list projects in your solution file.\n");
-            System.Console.WriteLine("dotnet test \n- Launch test runner for specified project. Returns 0 if successful. Returns 1 if failed.\n");
-            System.Console.WriteLine("dotnet clean \n- Cleans the output of the previous build.\n");            
+            new Command("git clone <CloneUrl>","Clone a local repo of the provided URL to your machine."),
+            new Command("git checkout <BranchToSwitchTo>","Switch to the branch provided. If you do not stage any changes made before switching branches, then your changes will switch with you. To prevent this you should stage changes before checkout to a new branch.\n"),
+            new Command("git add .","Stage all files in the current directory."),
+            new Command("git add <DirName>","Stage all files in the specified directory."),
+            new Command("git commit -m 'message'","Commit staged files so that they can be pushed."),
+            new Command("git push","Push all commited changes to the remote repo"),
+            new Command("git branch","List all branches in your local repo"),
+            new Command("git branch -a","Lists all branches even in the remote repo"),
+            new Command("git merge --abort","Cancel a pending merge"),
+            new Command("git remote set-url origin <url>","Reset the origin or remote repo")
+        };        
+        internal static List<Command> dotnetList = new List<Command>()
+        {           
+            new Command("dotnet new","add a new solution file to the current directory."),
+            new Command("dotnet new console","Create a new console project inside the current directory."),
+            new Command("dotnet build","Builds the project and dependencies to ensure you are up to date."),
+            new Command("dotnet run","Run your source code directly from the command prompt."),
+            new Command("dotnet sln","Sln allows you to add, remove, and list projects in your solution file."),
+            new Command("dotnet test","Launch test runner for specified project. Returns 0 if successful. Returns 1 if failed."),
+            new Command("dotnet clean","Cleans the output of the previous build.")
+        };
+        internal static void DisplayCommands(List<Command> commandsList)
+        {
+            foreach(Command cmd in commandsList)
+            {                
+            System.Console.WriteLine(cmd.name +"\n- " + cmd.desc + "\n");
+            }
         }
-
     }
 }
